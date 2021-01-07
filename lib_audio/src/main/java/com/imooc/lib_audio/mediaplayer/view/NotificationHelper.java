21ac046ef43fff7669ab24563148efce32da4ec4
zengx <mokeng_zx@163.com>
2021-01-07 10:57:04 +0800

    音乐Notification帮助类

---
+++ lib_audio/src/main/java/com/imooc/lib_audio/mediaplayer/view/NotificationHelper.java
@@ -0,0 +1,10 @@
+﻿package com.imooc.lib_audio.mediaplayer.view;
+
+/**
+ * @Description: 音乐Notification帮助类
+ * @Data: 2021/1/7
+ * @Author: zengx
+ * @UpdateRemark: 更新说明
+ */
+public class NotificationHelper {
+}
