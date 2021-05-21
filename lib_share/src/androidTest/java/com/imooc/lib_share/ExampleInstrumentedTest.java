ce34a2e1a8b23bd9870fb208548825f98ea1ca5c
zengx <mokeng_zx@163.com>
2021-05-21 10:46:00 +0800

    提交分享模块

--- lib_share/src/androidTest/java/com/imooc/lib_share/ExampleInstrumentedTest.java
+++ lib_share/src/androidTest/java/com/imooc/lib_share/ExampleInstrumentedTest.java
@@ -1,9 +1,8 @@
 package com.imooc.lib_share;
 
 import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,12 +14,11 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.imooc.lib_share.test", appContext.getPackageName());
-    }
+@RunWith(AndroidJUnit4.class) public class ExampleInstrumentedTest {
+  @Test public void useAppContext() {
+    // Context of the app under test.
+    Context appContext = InstrumentationRegistry.getTargetContext();
+
+    assertEquals("com.imooc.lib_share.test", appContext.getPackageName());
+  }
 }
